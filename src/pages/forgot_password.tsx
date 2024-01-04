d4657a7a5a2c17e0bda8bac454aced3eb7fd4de4
Sheimlly <sheimlly.dev@gmail.com>
2024-01-04 19:53:29 +0100

    auth form styled :);

--- src/pages/forgot_password.tsx
+++ src/pages/forgot_password.tsx
@@ -29,16 +29,27 @@ const ForgotPasswordForm = () => {
     }, [])
       
     return (
-        <section className="container login-page">
-            <h2 className="login-page--title">Forgot password</h2>
+        <section className="container forgot-password-page">
+            <div style={{height:'100vh'}} className="d-flex flex-column align-items-center justify-content-center">
+                <div className="auth-container">
+                    <h1 className="auth-container--title">Forgot password</h1>
 
-            <form onSubmit={(e) => handleSubmit(e)}>
-                <input name='email' ref={emailRef} type="text" placeholder="Email" required/>
-                <input type="submit" value="Send email" />
-            </form>
+                    <form className="auth-container__auth-form" onSubmit={(e) => handleSubmit(e)}>
+                        <input className="auth-container__auth-form--input" name='email' ref={emailRef} type="text" placeholder="Email" required/>
+                        <div className="auth-container__auth-form__submit-container">
+                            <input className="auth-container__auth-form__submit-container--button" type="submit" value="Send email" />
+                            <span className="auth-container__auth-form__submit-container--arrow-right arrow-right">
+                                <svg xmlns="http://www.w3.org/2000/svg" height="16" width="14" viewBox="0 0 448 512">
+                                    <path opacity="1" fill="#FFFFFF" d="M438.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-160-160c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L338.8 224 32 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l306.7 0L233.4 393.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l160-160z"/>
+                                </svg>
+                            </span>
+                        </div>
+                    </form>
 
-            <div style={emailSent ? {display:'block'} : {display:'none'}}>
-                <p>Email sent</p>
+                    <div className="auth-container--email-sent" style={emailSent ? {display:'block'} : {display:'none'}}>
+                        <p>Email sent</p>
+                    </div>
+                </div>
             </div>
         </section>
     )
