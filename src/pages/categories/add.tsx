53588dd2d6693f5ed7d3be3753c3bdbd2a90ca2c
Sheimlly <sheimlly.dev@gmail.com>
2024-01-08 19:31:51 +0100

    categories page styled and categories add form fixed

--- src/pages/categories/add.tsx
+++ src/pages/categories/add.tsx
@@ -28,16 +28,16 @@ const AddCategory = () => {
     }, [])
 
     return (
-        <section className='container form-page'>
-            <div className='form-page__container'>
-                <h1 className="form-page__container--title">Add Category</h1>
+        <section className='container form-section'>
+            <div className='form-section__container'>
+                <h1 className="form-section__container--title">Add Category</h1>
 
-                <form className='form-page__container__form' onSubmit={(e) => addCategory(e)}>
-                    <input className='form-page__container__form--input' type="text" ref={categoryName} placeholder='Category name' required />
-                    <input className='form-page__container__form--input' type="text" ref={categoryNamePl} placeholder='Category name pl' required />
-                    <div className='auth-container__auth-form__submit-container'>
-                        <input className='auth-container__auth-form__submit-container--button' type='submit' value='Add category' />
-                        <span className='auth-container__auth-form__submit-container--arrow-right arrow-right'>
+                <form className='form-section__container__form' onSubmit={(e) => addCategory(e)}>
+                    <input className='form-section__container__form--input' type="text" ref={categoryName} placeholder='Category name' required />
+                    <input className='form-section__container__form--input' type="text" ref={categoryNamePl} placeholder='Category name pl' required />
+                    <div className='form-section__container__form__submit-container'>
+                        <input className='form-section__container__form__submit-container--button' type='submit' value='Add category' />
+                        <span className='form-section__container__form__submit-container--arrow-right arrow-right'>
                             <svg xmlns="http://www.w3.org/2000/svg" height="16" width="14" viewBox="0 0 448 512">
                                 <path opacity="1" fill="#FFFFFF" d="M438.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-160-160c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L338.8 224 32 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l306.7 0L233.4 393.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l160-160z"/>
                             </svg>
--- src/pages/categories/index.tsx
+++ src/pages/categories/index.tsx
@@ -62,26 +62,44 @@ const CategoriesPage = () => {
                 <h1 className="site_header-title">Categories</h1>
             </section>
 
-            <section className="container">
-            <div className="add-instance"><a href='/categories/add'>+ Add Category</a></div>
-                <div className="filtering row my-5">
-                    <div className="col-3">
-                        <p>Category name</p>
-                        <input onChange={(e) => { setSearch(e.target.value) }} type="text" value={search} />
+            <section className="filters-section container">
+                <div className='filters-section__container'>
+                    <h3 className='filters-section--title'>Filters</h3>
+
+                    <div className='filters-section__container__filters'>
+                        <input className='filters-section__container__filters--input' onChange={(e) => { setSearch(e.target.value) }} type="text" value={search} placeholder='Name'/>
+                        <button className='filters-section__container__filters--button'><a href='/categories/add'>Add Category</a></button>
+                        {/* <div className='filters-section__container__filters__add-button'>
+                            <button className='filters-section__container__filters__add-button--button'>Add category</button>
+                            <span className='filters-section__container__filters__add-button--add-icon add-icon'>
+                                <svg fill="#FFFFFF" version="1.1" id="Capa_1" viewBox="0 0 45.402 45.402">
+                                    <g>
+                                    <path d="M41.267,18.557H26.832V4.134C26.832,1.851,24.99,0,22.707,0c-2.283,0-4.124,1.851-4.124,4.135v14.432H4.141
+                                        c-2.283,0-4.139,1.851-4.138,4.135c-0.001,1.141,0.46,2.187,1.207,2.934c0.748,0.749,1.78,1.222,2.92,1.222h14.453V41.27
+                                        c0,1.142,0.453,2.176,1.201,2.922c0.748,0.748,1.777,1.211,2.919,1.211c2.282,0,4.129-1.851,4.129-4.133V26.857h14.435
+                                        c2.283,0,4.134-1.867,4.133-4.15C45.399,20.425,43.548,18.557,41.267,18.557z"/>
+                                    </g>
+                                </svg>
+                            </span>
+                        </div> */}
                     </div>
                 </div>
 
-                <div className="categories_sessions-list">
+            </section>
+
+            <section className='categories-section container my-5'>
+                <div className='categories-section__container row justify-content-between'>
                     {categories.map(category => {
                         return(
-                            <div className="row mb-3">
-                                <p className="col-2">{category.name}</p>
-                                <p className="col-1">-</p>
-                                <p className="col-2">{category.name_pl}</p>
-                                <button className="col-2 a-button"><a href={`/categories/${category.id}`}>edit</a></button>
-                                <p className="col-1"> | </p>
+                            <div className='categories-section__container__category col-4 my-3'>
+                                <div className='categories-section__container__category--text'>
+                                    <p>{category.name}</p>
+                                    <span> - </span>
+                                    <p>{category.name_pl}</p>
+                                </div>
+                                <a className='categories-section__container__category--button' href={`/categories/${category.id}`}>Edit</a>
                                 { category.able_to_delete &&
-                                    <button className="col-2" onClick={() => {handleDelete(category.id)}}>Delete</button>
+                                    <p className='categories-section__container__category--delete' onClick={() => {handleDelete(category.id)}}>Delete</p>
                                 }
                             </div>
                         )
